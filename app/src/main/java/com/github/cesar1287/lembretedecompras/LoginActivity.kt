bb731cfefb6ecea8df6c2b5c244527c1a4a90f06
Cesar Rodrigues <cesar.nascimento.ufc@gmail.com>
2023-03-07 19:28:12 -0300

    login activity

--- app/src/main/java/com/github/cesar1287/lembretedecompras/LoginActivity.kt
+++ app/src/main/java/com/github/cesar1287/lembretedecompras/LoginActivity.kt
@@ -2,12 +2,12 @@ package com.github.cesar1287.lembretedecompras
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.github.cesar1287.lembretedecompras.databinding.ActivityMainBinding
+import com.github.cesar1287.lembretedecompras.databinding.ActivityLoginBinding
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
-    private val binding: ActivityMainBinding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
+    private val binding: ActivityLoginBinding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
