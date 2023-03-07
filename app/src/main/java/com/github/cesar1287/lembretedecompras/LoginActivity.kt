2b14498cb04f23faf83f2a04f9af4df3e0b77104
Cesar Rodrigues <cesar.nascimento.ufc@gmail.com>
2023-03-07 20:03:28 -0300

    adding init and keyboard functions

--- app/src/main/java/com/github/cesar1287/lembretedecompras/LoginActivity.kt
+++ app/src/main/java/com/github/cesar1287/lembretedecompras/LoginActivity.kt
@@ -2,9 +2,15 @@ package com.github.cesar1287.lembretedecompras
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.github.cesar1287.lembretedecompras.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var animacaoDoMascote: Animation
+    private lateinit var animacaoDoFormulario: Animation
 
     private val binding: ActivityLoginBinding by lazy {
         ActivityLoginBinding.inflate(layoutInflater)
@@ -13,5 +19,25 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        initAnimation()
+
+        hideKeyboard()
     }
+
+    private fun initAnimation() {
+        with(binding) {
+            animacaoDoMascote = AnimationUtils.loadAnimation(this@LoginActivity, R.anim.frombottom2)
+            animacaoDoFormulario = AnimationUtils.loadAnimation(this@LoginActivity, R.anim.frombottom)
+            ivLogin.clearAnimation()
+            containerLogin.clearAnimation()
+            containerLogin.startAnimation(animacaoDoFormulario)
+            ivLogin.startAnimation(animacaoDoMascote)
+        }
+    }
+
+    private fun hideKeyboard() {
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN)
+    }
+
 }
