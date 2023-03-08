c83576e276eb41ee6e6ac6e9a58e06f9afc3c6c2
Cesar Rodrigues <cesar.nascimento.ufc@gmail.com>
2023-03-08 08:17:04 -0300

    fisinishing app

--- app/src/main/java/com/github/cesar1287/lembretedecompras/ui/main/MainActivity.kt
+++ app/src/main/java/com/github/cesar1287/lembretedecompras/ui/main/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.github.cesar1287.lembretedecompras.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.github.cesar1287.lembretedecompras.databinding.ActivityMainBinding
+import com.github.cesar1287.lembretedecompras.model.Product
+import com.github.cesar1287.lembretedecompras.ui.newproduct.NewProductActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,8 +17,52 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private val mainViewModel: MainViewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
+
+    private val mainListAdapter: MainListAdapter by lazy {
+        MainListAdapter()
+    }
+
+    private val newProductRequest =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == RESULT_OK) {
+                it.data?.getStringExtra(NewProductActivity.EXTRA_REPLY)?.let { productName ->
+                    val product = Product(productName)
+                    mainViewModel.insert(product)
+                }
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        setUpRecyclerView()
+        initObserver()
+        initListeners()
     }
+
+    private fun initListeners() {
+        binding.fabNewProduct.setOnClickListener {
+            val nextScreen = Intent(this, NewProductActivity::class.java)
+            newProductRequest.launch(nextScreen)
+        }
+    }
+
+    private fun initObserver() {
+        mainViewModel.products.observe(this) { products ->
+            products?.let { mainListAdapter.submitList(products) }
+        }
+    }
+
+    private fun setUpRecyclerView() {
+        with(binding) {
+            rvProducts.adapter = mainListAdapter
+            rvProducts.layoutManager = LinearLayoutManager(this@MainActivity)
+        }
+    }
+
+
 }
